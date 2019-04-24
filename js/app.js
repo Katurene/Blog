7f07350ca21410c54cf8fc8be74ce80d32caa999
Katurene <katurene7@gmail.com0>
2019-04-24 23:12:06 +0300

    JQuery (сделана выпадающая шапка)

--- js/app.js
+++ js/app.js
@@ -3,11 +3,24 @@
  */
 $(function () {
     var header = $("#header"),
-        introH = $("#intro").innerHeight();
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+    checkScroll(scrollOffset);
 
     $(window).on("scroll", function () {
 
+        scrollOffset = $(this).scrollTop();
 
-        console.log(introH);
+        checkScroll(scrollOffset);
     });
+
+    function checkScroll(scrollOffset) {
+
+        if (scrollOffset >= introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
 });
